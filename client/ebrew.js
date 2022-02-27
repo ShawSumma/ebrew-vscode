5c469f970e6bcb82f9ab6ce840e5bb69a6dd7ded
Shaw Summa <shawsumma@gmail.com>
2022-02-27 12:04:29 -0500

    idents but smart

--- client/ebrew.js
+++ client/ebrew.js
@@ -55,6 +55,35 @@ const walkHover = (node, needle, types = {}) => {
     return null;
 };
 
+const walkLocal = (node, cb, types={}, func=false) => {
+    if (node instanceof Form) {
+        let next = types;
+        if (node.form === 'func') {
+            next = { ...next };
+            for (let i = 1; i < node.args.length - 1; i++) {
+                const arg = node.args[i];
+                next[arg.args[0].repr] = arg;
+            }
+        }
+        if (node.form === 'let') {
+            next = { ...next };
+            next[node.args[0].repr] = new Form('tvalue', node.args[0]);
+        }
+        if (node.form === 'call') {
+            walkLocal(node.args[0], cb, next, true);
+            for (let arg of node.args.slice(1)) {
+                walkLocal(arg, cb, next, false);
+            }
+        } else {
+            for (let arg of node.args) {
+                walkLocal(arg, cb, next, false);
+            }
+        }
+    } else if (node instanceof Ident) {
+        cb(func, node);
+    }
+};
+
 const activate = () => {
     vscode.languages.registerHoverProvider('ebrew', {
         provideHover: (doc, pos) => {
@@ -101,7 +130,6 @@ const activate = () => {
         provideDocumentSymbols: (doc) => {
             const parser = new Parser(doc.getText());
             parser.raise = (...args) => {
-                // throw new ParseError(parser.state.line, parser.state.col, args);
                 return new Ident('?');
             };
             const prog = parser.readDefs();
@@ -113,10 +141,42 @@ const activate = () => {
                 const range = new vscode.Range(toPos(def.start), toPos(def.end));
                 ret.push(new vscode.DocumentSymbol(def.args[0].repr, def.form, vscode.SymbolKind.Function, range, range));
             }
-            // console.log(ret);
             return ret;
         }
     });
+    const tokenTypes = ['function', 'variable'];
+    const tokenModifiers = ['declaration'];
+    const legend = new vscode.SemanticTokensLegend(tokenTypes, tokenModifiers);
+    const provideDocumentSemanticTokens = (doc) => {
+        const builder = new vscode.SemanticTokensBuilder(legend);
+        const parser = new Parser(doc.getText());
+        parser.raise = (...args) => {
+            return new Ident('?');
+        };
+        const prog = parser.readDefs();
+        const globals = {};
+        for (const def of prog) {
+            if (def.form === 'func') {
+                globals[def.args[0].repr] = new Form('tfunc', def.args[0], def.args.slice(1, -1));
+            }
+            if (def.form === 'extern') {
+                globals[def.args[0].repr] = new Form('textern', def.args[0], def.args.slice(1));
+                continue;
+            }
+            walkLocal(def, (type, ident) => {
+                if (ident.start == null || ident.end == null) {
+                    return null;
+                }
+                if (type) {
+                    builder.push(new vscode.Range(toPos(ident.start), toPos(ident.end)), 'variable', []);
+                } else {
+                    builder.push(new vscode.Range(toPos(ident.start), toPos(ident.end)), 'function', []);
+                }
+            }, globals);
+        }
+        return builder.build();
+    };
+    vscode.languages.registerDocumentSemanticTokensProvider('ebrew', { provideDocumentSemanticTokens }, legend);
 };
 
 const deactivate = () => {
